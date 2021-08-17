a7296531d5340d2004d5bb38ce0f8fc64ef4c83d
Ambrose Karella <ajkarella@gmail.com>
2021-08-17 18:46:50 -0400

    added func to index btns, saves query as text var, keeps search bar populated w/ query after submission

--- esFront/app.py
+++ esFront/app.py
@@ -1,8 +1,18 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    return render_template('search.html')
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def getQuery():
+    if request.method == "POST":
+        if request.form['searchBtn'] == 'Gaamle Search':
+            text = request.form['text']
+            print("gamle search - ",text)
+        elif request.form['searchBtn'] == "I'm Feeling Lucky":
+            text = request.form['text']
+            print("feelin lucky - ", text)
+    return render_template('index.html',text=text)
