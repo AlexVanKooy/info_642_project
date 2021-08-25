cf42f9cebfca48cd7c824856c5350b6093cc4ded
Alex Van Kooy <avankooy@gmail.com>
2021-08-25 15:35:23 -0400

    adjusted payload format to get rid of the 'doc' prefix

--- retrieval_data_upload.py
+++ retrieval_data_upload.py
@@ -1,8 +1,10 @@
 import json
 import os
 import ssl
+import keyring
 import requests
-from elasticsearch import Elasticsearch
+from dateutil import parser
+from elasticsearch import Elasticsearch, helpers
 from elasticsearch.connection import create_ssl_context
 from requests.auth import HTTPBasicAuth
 
@@ -36,7 +38,7 @@ def drop_empty_data(j_data, save_file=True):
         json.dump(collected_games,fout)
 
 
-def collect_and_set_mapped_fields(j_data, mapped_fields):
+def collect_and_set_mapped_fields(j_data, mapped_fields, write_to_file=False):
     
     final_data = {}
     
@@ -45,25 +47,46 @@ def collect_and_set_mapped_fields(j_data, mapped_fields):
         if game[0] not in final_data.keys():
             for field in mapped_fields:
                 if game[1][field] != 'Data Not Available':
-                    temp_dict[field] = game[1][field]
+                    if field == "initial_release_date":
+                        temp_date = parser.parse(game[1][field])
+                        temp_dict[field] = str(temp_date.date())
+                    else:
+                        temp_dict[field] = game[1][field]
                 else:
                     temp_dict[field] = None
             final_data[game[0]] = temp_dict
         else:
             continue
+    if write_to_file == True:
+        with open("final_data_payload.json","w") as data_out:
+            json.dump(final_data,data_out)
     return final_data
     
-def make_ES_payload(j_data):
-    doc_counter = 1
+def make_ES_payload(j_data, target_index):
+    doc_counter = 0
+    doc_list = []
     for entry in j_data.values():
-        yield{
-            "_index":"av667_info624_202004_project",
-            "_type":"document",
-            "_id":doc_counter,
-            'doc':entry}
+        doc_counter+=1
+
+        item = [{"index":{"_index":target_index,"_id":doc_counter}},
+                entry]
+        doc_list.extend(item)
+    return doc_list
 
                     
-            
+                    
+def build_actions(j_data, target_index):
+    
+    actions = [
+    {
+        '_index': target_index,
+        # '_type': 'document',
+        '_id': i,
+        '_source': v,
+    }
+    for i, (k, v) in enumerate(j_data.items(), 1)
+    ]
+    return actions         
         
 
 def main():
@@ -71,24 +94,38 @@ def main():
     context.check_hostname = False
     context.verify_mode = ssl.CERT_NONE
     es = Elasticsearch(['tux-es1.cci.drexel.edu','tux-es2.cci.drexel.edu','tux-es3.cci.drexel.edu'],
-                       http_auth=('user','secret'), # working on real encrypted passwords
+                       http_auth=('av667',keyring.get_password('elastic',"av667")), # working on real encrypted passwords
                        scheme="https",
                         port=9200,
-                        ssl_context = context)
+                        ssl_context = context,
+                        timeout=30, max_retries=10, retry_on_timeout=True)
     
-    # print(es.info())
+    
     
                        
     mapped_fields = ["title", "developer","type","description", 
                      "source", "editorial_reviews", "initial_release_date","platforms",
                      "composer","publisher","series"]
-    #Already trimmed the file to only have the games with actual data
+    
+    ###############
+    # If you do not have the "jdata_minus_empties.json" file yet and have the original data, run the following
+    ########
     # drop_empty_data(game_data, True)
+    
     game_data = get_json_data('jdata_minus_empties.json')
     
-    game_data = collect_and_set_mapped_fields(game_data, mapped_fields)
-    one_payload = make_ES_payload(game_data)
-    print('ready to start uploading?')
+    game_data = collect_and_set_mapped_fields(game_data, mapped_fields, True)
+    
+    actions = build_actions(game_data, target_index="av667_info624_202004_project_no_doc_prefix")
+    
+    
+    try:
+        stats=helpers.bulk(es,actions)
+        # es.bulk(actions)
+    except Exception as e:
+        print("something broke")
+        print(e)
+    
 
 
 
